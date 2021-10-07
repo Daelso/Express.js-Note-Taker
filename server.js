c35570f726cee5f0298410809492ad7ab1239554
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-10-06 23:18:28 -0400

    Adds pathing to index page and notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const path = require('path');
 
 const uuid = require('./helpers/uuid');
 
@@ -9,7 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('Develop/public'));
 
-
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+);
 
 const PORT = 3001
 app.listen(PORT, () =>
