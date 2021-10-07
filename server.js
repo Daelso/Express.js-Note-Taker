ffdca5ca663f7971f776dccfd62850237e220381
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-10-06 23:10:35 -0400

    static index page, some initial setup and installs

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express()
 
+const uuid = require('./helpers/uuid');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('Develop/public'));
 
 
 
-
-
-
-
-
+const PORT = 3001
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
