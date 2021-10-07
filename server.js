b612a4c7132b3ee7fe3871bb5d49c1efdb22b690
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-10-07 01:19:58 -0400

    get and post statements for /api notes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express()
+const app = express();
 const path = require('path');
+const db = require("./Develop/db/db.json")
 
 const uuid = require('./helpers/uuid');
 
@@ -13,6 +14,17 @@ app.use(express.static('Develop/public'));
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) =>
+res.status(200).json(db));
+
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a review`);
+
+
+})
+
 
 const PORT = 3001
 app.listen(PORT, () =>
